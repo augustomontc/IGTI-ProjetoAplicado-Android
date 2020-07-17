0c81f42629be47811f5ea32c6aca95c30eeef009
augustomontc <augustocosta2609@gmail.com>
2020-07-16 21:24:46 -0300

    Adicionando limite de um acesso à SplashScreen

--- app/src/main/java/com/augustomontc/igtipaa/activities/SplashActivity.kt
+++ app/src/main/java/com/augustomontc/igtipaa/activities/SplashActivity.kt
@@ -1,22 +1,37 @@
 package com.augustomontc.igtipaa.activities
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import com.augustomontc.igtipaa.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
+        val settings = getSharedPreferences("prefs", Context.MODE_PRIVATE)
+        val firstRun = settings.getBoolean("firstRun", false)
+        if (!firstRun) {
+            val editor = settings.edit()
+            editor.putBoolean("firstRun", true)
+            editor.apply()
+            setContentView(R.layout.activity_splash)
+            startTimer()
+        } else {
+            startMainActivity()
+        }
+
+    }
+
+    // Actions
+    private fun startTimer() {
         Handler().postDelayed({
             startMainActivity()
         }, 4000)
     }
 
-    // Actions
     private fun startMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
